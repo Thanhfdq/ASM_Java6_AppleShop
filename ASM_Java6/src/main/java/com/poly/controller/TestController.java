cb4ab495e011e8ffbd1c8b18b726502a17e965b2
thanh <tp.thanhjava@gmail.com>
2023-07-31 14:28:43 +0700

    commit thử

--- ASM_Java6/src/main/java/com/poly/controller/TestController.java
+++ ASM_Java6/src/main/java/com/poly/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
+	
+	//test commit
     @RequestMapping("/index")
     public String test(){
         return "user/blog";
